a0c429f7aa798c22dfcbb6ed83566d1d3536b5ac
Zhosan Alice <v.josan@mail.ru>
2019-10-15 17:26:19 +0300

    Added Probnoe

---
+++ probnoe.py
@@ -0,0 +1,23 @@
+from graph import *
+
+brushColor("yellow")
+circle(250, 250, 100)
+
+brushColor("red")
+circle(300, 225, 15)
+circle(200, 225, 20)
+
+brushColor("black")
+circle(200, 225, 8)
+circle(300, 225, 8)
+
+brushColor("black")
+polygon ([ (280, 220), (275, 210), (340, 175), (345,185) ])
+
+brushColor("black")
+polygon ([ (225, 215), (230, 205), (150, 170), (145,180) ])
+
+brushColor("black")
+rectangle(200, 300, 300, 320)
+
+run()
